030bbfb71d5d077f82643f4e26eab0833f880a70
Ian Barrick <ian@servisuite.com>
2022-09-27 09:10:56 -0400

    nixpkgs suddenly works?

--- flake.nix
+++ flake.nix
@@ -9,8 +9,7 @@
       pkgs = import nixpkgs { system = "x86_64-linux"; };
     in
     {
-# can't get this to work anymore?
-#      hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
       ghostpdl = import ./ghostpdl { stdenv = pkgs.stdenv; };
     };
